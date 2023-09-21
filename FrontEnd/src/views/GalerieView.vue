6b50c69bc4063205a08b2dce5bb8abe956072aea
geek2tech <geek2tech@geek2tech.Fr>
2023-09-21 21:51:47 +0200

    add filter on galeryView ( not functionnal yet , just design and retreive data from BackEnd )

--- FrontEnd/src/views/GalerieView.vue
+++ FrontEnd/src/views/GalerieView.vue
@@ -1,12 +1,30 @@
 <template>
   <div class="GaleryPage container">
-<h1>Galerie</h1>
-<div class="photoContainer row d-flex flex-wrap justify-content-around align-items-baseline">
-<div v-for="photo in photos" :key="photo.id" class="col-lg-4 col-sm-6  p-3">
-   <PhotoComponent :categorie="photo[0]" :url="photo[1]" :description=photo[2] />
-</div>
+    <h1>Galerie</h1>
+    <div class="row">
 
-</div>
+      <div class="dropdown">
+        <button class="btn btn-outline-success btn-lg dropdown-toggle"
+                type="button" id="dropdownMenuButton1"
+                data-bs-toggle="dropdown"
+                aria-expanded="false">
+          Filtrer les photos
+        </button>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+          <li v-for="categorie in categories" :key="categorie.id">
+            <button class="dropdown-item">{{ categorie }}</button>
+          </li>
+
+
+        </ul>
+      </div>
+    </div>
+    <div class="photoContainer row d-flex flex-wrap justify-content-around align-items-baseline">
+      <div v-for="photo in photos" :key="photo.id" class="col-lg-4 col-sm-6  p-3">
+        <PhotoComponent :categorie="photo[0]" :url="photo[1]" :description=photo[2] />
+      </div>
+
+    </div>
   </div>
 
 </template>
@@ -17,8 +35,6 @@ import PhotoComponent from "@/components/Photo-Component.vue";
 import axios from 'axios'
 
 
-
-
 export default {
   name: 'Galerie-View',
   components: {PhotoComponent},
@@ -26,14 +42,15 @@ export default {
   data() {
     return {
 
-      photos: []
+      photos: [],
+      categories: []
     }
 
   },
   mounted() {
-    const requete = 'http://localhost:1337/api/photos?fields[0]=Description&populate[Photo][fields][0]=url&populate[categorie][fields][1]=categoryName'
+    const requetePhoto = 'http://localhost:1337/api/photos?fields[0]=Description&populate[Photo][fields][0]=url&populate[categorie][fields][1]=categoryName'
     axios
-        .get(requete)
+        .get(requetePhoto)
         .then((response) => {
           let responseArray = Object.values(response.data)
 
@@ -42,18 +59,26 @@ export default {
             const url = process.env.VUE_APP_STRAPI_URL + item.attributes.Photo.data.attributes.url
             const categorie = item.attributes.categorie.data.attributes.categoryName
             const description = item.attributes.Description
-            console.log(url)
-            console.log(categorie)
-            this.photos.push([categorie,url,description])
-
-
+            this.photos.push([categorie, url, description])
           })
-          console.log(this.photos)
-          console.log(this.photos[2][0])
-          console.log(this.photos[2][1])
-
-
         })
+        .catch(()=>{
+          alert('une erreur est survenu dans la récupération des données')
+        })
+    const requeteCategory = 'http://localhost:1337/api/categories?fields[0]=categoryName'
+    axios
+        .get(requeteCategory)
+        .then((response) => {
+          let responseArray = Object.values(response.data)
+          responseArray[0].forEach((item) => {
+            const categoryName = item.attributes.categoryName
+            this.categories.push(categoryName)
+            })
+        })
+        .catch(()=>{
+          alert('une erreur est survenu dans la récupération des données')
+        })
+
   },
 
 }
@@ -62,11 +87,12 @@ export default {
 </script>
 
 <style scoped>
-div{
-  color:white;
+div {
+  color: white;
 }
-li{
-  color:white;
+
+li {
+  color: white;
 }
 
 </style>
